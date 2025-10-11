81df021636a298f48c672c6f35b8fe80d23b1d36
Shah Newaj <newajuiucse@gmail.com>
2025-10-11 10:55:32 +0600

    prints test data

--- Week 1 - Task 1/step2_variables.py
+++ Week 1 - Task 1/step2_variables.py
@@ -1,4 +1,7 @@
-username = "newaj"
-password = "1234"
-print("Username is:", username)
-print("Password is:", password)
+username = input("Enter username: ")
+password = input("Enter password: ")
+print(f"Username: {username}, Password: {password}")
+
+# name = "newaj"
+# passw = "1234"
+# print(name, passw)
