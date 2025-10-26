24bed18f83f732e8b85796dd3da976829b5b2d00
Shah Newaj <newajuiucse@gmail.com>
2025-10-26 11:08:43 +0600

    Example 2 – Text Content Validation

--- Week 3 - Task 3/test_login_button_text.py
+++ Week 3 - Task 3/test_login_button_text.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Page, expect
 
 # Example 2 – Text Content Validation
 
-def test_page_title(playwright):
+def test_login_button_text(playwright):
     browser = playwright.chromium.launch(headless=False, args=["--start-maximized"])
     print("Launching Chrome browser ..............")
     context = browser.new_context(no_viewport=True)
@@ -11,6 +11,12 @@ def test_page_title(playwright):
     page.goto("https://practicetestautomation.com/practice-test-login/")
     button_text = page.locator("#submit")
     # assert button_text == "Submit" # General Python approach
-    expect(button_text).to_have_text("Submit") # Playwright built-in assertion mechanism
-    print("Button text successfully matches")
+    # expect(button_text).to_have_text("Submit") # Playwright built-in assertion mechanism
+
+    try:
+        expect(button_text).to_have_text("Submit") # Playwright built-in assertion mechanism
+        print("✅ Button Text matched successfully!")
+    except AssertionError as e:
+        print("❌ Button Text did not match!")
+        print("Error details:", e)
     browser.close()
